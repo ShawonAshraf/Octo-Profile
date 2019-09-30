47b83fb41b0faeba147bf3a66484bd92f7f1eacf
Shawon Ashraf <shawon13@live.com>
2019-10-01 03:20:58 +0600

    init

---
+++ Octo-Profile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Octo-Profile
+//
+//  Created by Shawon Ashraf on 10/1/19.
+//  Copyright © 2019 Shawon Ashraf. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
